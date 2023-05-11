4107731f1154ad26daef8326a08b2f4b33caff33
alex mutisya <alexrichamerican@gmail.com>
2023-05-11 12:50:34 +0300

    valuables
    valuables in kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    val a=40
+     val b=45
+    val product=(a*b)
+    println("$product")
+
+
+    val x=1500
+    val y=150
+    val divider=(x/y)
+    println("$divider")
+
+
+
+val m=600
+    val n=2.24f
+    val sum=(m+n)
+    println("$sum")
+
+
+
+
+
+
+}
